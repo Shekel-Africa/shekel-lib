25c7c9b5fc35dee7d456a6381b77f02415628bd3
Muheez Jimoh <jamuheez2009@gmail.com>
2022-12-16 09:05:45 +0100

    updated the link wallet to properly link to the right user

--- src/Services/TransactionService.php
+++ src/Services/TransactionService.php
@@ -18,12 +18,16 @@ class TransactionService extends ShekelBaseService {
         ]);
     }
 
-    public function createProvidusAccount($email, $bvn) {
+    public function createProvidusAccount($email, $bvn, $user_id=null) {
         $url = "/wallet/link/bank";
-        return $this->client->post($url, [
+        $data = [   
             'email' => $email,
             'bvn' => $bvn
-        ]);
+        ];
+        if (isset($user_id)) {
+            $data['user_id'] = $user_id;
+        }
+        return $this->client->post($url, $data);
     }
 
     public function updateWalletType($currency='NGN'){
