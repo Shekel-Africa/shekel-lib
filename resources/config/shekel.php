87ec1041d6e2f45c1cad5cd3d460db0abc8676ec
Muheez Jimoh <jamuheez2009@gmail.com>
2023-01-13 01:12:48 +0100

    added middleware to add service name to headers

--- resources/config/shekel.php
+++ resources/config/shekel.php
@@ -8,4 +8,5 @@ return [
     'messaging' => getenv('MESSAGING_SERVICE'),
     'upload' => getenv('UPLOAD_SERVICE'),
     'transaction' => getenv('TRANSACTION_SERVICE'),
+    'service_name' => getenv('SERVICE_NAME'),
 ];
---
+++ src/Middleware/AddServiceNameToHeader.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class AddServiceNameToHeader {
+
+     /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $request->headers->set('Service-Name', Config::get("shekel.service_name"));
+        $response = $next($request);
+        return $response;
+    }
+}
