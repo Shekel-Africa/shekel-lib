da54647a5ba1b21643461392e04a968f1a4d82d8
Muheez Jimoh <jamuheez2009@gmail.com>
2023-03-21 21:28:13 +0100

    fixed issue with cache

--- src/Utils/PassportToken.php
+++ src/Utils/PassportToken.php
@@ -27,7 +27,7 @@ class PassportToken
             abort(400, "Client Secrets Missing");
         }
         $token = $request->header('x-auth');
-        return Cahce::remember($token, '144404', function () use($token) {
+        return Cache::remember($token, '144404', function () use($token) {
             $arr = explode(':', base64_decode($token));
             if (count($arr) !== 2) {
                 abort(400, "Invalid client secrets");
