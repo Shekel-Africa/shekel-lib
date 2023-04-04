1fd3c1cc4d6b43e792c56a9ed0cbd0053b9c955b
Muheez Jimoh <jamuheez2009@gmail.com>
2023-04-04 14:27:22 +0100

    rolled back changes to auth

--- src/Middleware/ServiceAuthentication.php
+++ src/Middleware/ServiceAuthentication.php
@@ -22,13 +22,23 @@ class ServiceAuthentication
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next, ...$scopes)
     {
         try {
             $this->authService->setToken($request->bearerToken());
             $this->authService->verifyToken($scopes);
+            $userCheck = $this->authService->getAuthenticated(['excludeImage'=>true]);
+            if (!$userCheck->successful()) {
+                return response()->json($userCheck->json(), $userCheck->status());
+            }
+            $user = $userCheck->json('data');
+            //kyc not completed
+            if (!empty($scopes) && !$user['kyc_complete']) {
+                return response()->json(['message' => 'Kyc not completed'], 403);
+            }
+            Auth::guard()->setUser(new GenericUser($user));
             return $next($request);
         } catch(\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], $th->getStatusCode());
