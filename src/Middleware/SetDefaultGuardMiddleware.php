d2af2f26033a1c37c8c9cc86eb53ceae26df0c6d
Muheez Jimoh <jamuheez2009@gmail.com>
2023-08-18 09:38:02 +0100

    added middleware

--- src/Middleware/SetDefaultGuardMiddleware.php
+++ src/Middleware/SetDefaultGuardMiddleware.php
@@ -2,7 +2,22 @@
 
 namespace Shekel\ShekelLib\Middleware;
 
-class SetDefaultGuard
-{
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+class SetDefaultGuardMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $guard)
+    {
+        Auth::shouldUse($guard);
+        return $next($request);
+    }
 }
