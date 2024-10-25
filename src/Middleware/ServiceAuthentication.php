a30a0bde103b5d53eb1c6e4e07e5fbb26d39b325
Muheez Jimoh <jamuheez2009@gmail.com>
2024-10-25 12:43:03 +0100

    Fixed issue with error handler

--- src/Middleware/ServiceAuthentication.php
+++ src/Middleware/ServiceAuthentication.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Auth\GenericUser;
 use Illuminate\Http\Response;
+use Shekel\ShekelLib\Exceptions\ShekelInvalidArgumentException;
 use Shekel\ShekelLib\Services\AuthService;
 use Illuminate\Support\Facades\Auth;
 use Shekel\ShekelLib\Utils\ShekelAuth;
@@ -37,7 +38,13 @@ class ServiceAuthentication
             }
             return $next($request);
         } catch(\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()], $th->getStatusCode());
+            if ($th instanceof ShekelInvalidArgumentException) {
+                return response()->json(['message' => $th->getMessage()], $th->getCode());
+            }
+            if ($th instanceof  \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface) {
+                return response()->json(['message' => $th->getMessage()], $th->getCode() ?? $th->getStatusCode());
+            }
+            return response()->json(['message' => $th->getMessage()], 400);
         }
     }
 }
