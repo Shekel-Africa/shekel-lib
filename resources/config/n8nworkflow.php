62e9d7f68b6cbf5fc06af2d414b90db61069bfbd
Muheez Jimoh <jamuheez2009@gmail.com>
2025-04-02 11:09:13 +0100

    Added n8nworkflow notifier

---
+++ resources/config/n8nworkflow.php
@@ -0,0 +1,5 @@
+<?php
+// n8n workflow configuration
+return [
+    'baseUrl' => env('N8N_WORKFLOW_BASE_URL', 'https://igbese.shekel.africa')
+];
---
+++ src/Utils/N8NWorkflowNotify.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Shekel\ShekelLib\Utils;
+
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+
+class N8NWorkflowNotify
+{
+    protected PendingRequest|Http $client;
+
+    /**
+     * @param string $path
+     * @param array $data
+     * @return PromiseInterface|Response|null
+     * @throws ConnectionException
+     */
+    public static function sendEvent(string $path, array $data): PromiseInterface|Response|null
+    {
+        $env = strtolower(Config::get("app.env"));
+        if (in_array($env, ['testing', 'local'])) {
+            return null;
+        }
+        $url = match ($env){
+            'production' => Config::get('n8nworkflow.baseUrl') . "/webhook/$path",
+            default => Config::get('n8nworkflow.baseUrl') . "/webhook-test/$path"
+        };
+        $headers = [
+            'Accept' => '*/*',
+            'Content-Type' => 'application/json',
+        ];
+        $data['client_id'] = TenantClient::getClientId();
+        return Http::withHeaders($headers)->post($url, $data);
+    }
+}
