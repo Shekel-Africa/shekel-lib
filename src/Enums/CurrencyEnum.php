98665822fca4cedebf06d669bbbb91b0459f23c9
Muheez Jimoh <jamuheez2009@gmail.com>
2025-05-05 12:00:22 +0100

    added currency enum to shekel lib

---
+++ src/Enums/CurrencyEnum.php
@@ -0,0 +1,353 @@
+<?php
+
+namespace Shekel\ShekelLib\Enums;
+
+class CurrencyEnum
+{
+    const USD = 'USD';
+    const KES = 'KES';
+    const GBP = 'GBP';
+    const CAD = 'CAD';
+    const EUR = 'EUR';
+    const JPY = 'JPY';
+    const AUD = 'AUD';
+    const CHF = 'CHF';
+    const CNY = 'CNY';
+    const SEK = 'SEK';
+    const NZD = 'NZD';
+    const MXN = 'MXN';
+    const SGD = 'SGD';
+    const HKD = 'HKD';
+    const NOK = 'NOK';
+    const KRW = 'KRW';
+    const TRY = 'TRY';
+    const RUB = 'RUB';
+    const INR = 'INR';
+    const BRL = 'BRL';
+    const ZAR = 'ZAR';
+    const AED = 'AED';
+    const AFN = 'AFN';
+    const ALL = 'ALL';
+    const AMD = 'AMD';
+    const ANG = 'ANG';
+    const AOA = 'AOA';
+    const ARS = 'ARS';
+    const AWG = 'AWG';
+    const AZN = 'AZN';
+    const BAM = 'BAM';
+    const BBD = 'BBD';
+    const BDT = 'BDT';
+    const BGN = 'BGN';
+    const BHD = 'BHD';
+    const BIF = 'BIF';
+    const BMD = 'BMD';
+    const BND = 'BND';
+    const BOB = 'BOB';
+    const BOV = 'BOV';
+    const BSD = 'BSD';
+    const BTN = 'BTN';
+    const BWP = 'BWP';
+    const BYN = 'BYN';
+    const BZD = 'BZD';
+    const CDF = 'CDF';
+    const CHE = 'CHE';
+    const CHW = 'CHW';
+    const CLF = 'CLF';
+    const CLP = 'CLP';
+    const COP = 'COP';
+    const COU = 'COU';
+    const CRC = 'CRC';
+    const CUC = 'CUC';
+    const CUP = 'CUP';
+    const CVE = 'CVE';
+    const CZK = 'CZK';
+    const DJF = 'DJF';
+    const DKK = 'DKK';
+    const DOP = 'DOP';
+    const DZD = 'DZD';
+    const EGP = 'EGP';
+    const ERN = 'ERN';
+    const ETB = 'ETB';
+    const FJD = 'FJD';
+    const FKP = 'FKP';
+    const FOK = 'FOK';
+    const GEL = 'GEL';
+    const GGP = 'GGP';
+    const GHS = 'GHS';
+    const GIP = 'GIP';
+    const GMD = 'GMD';
+    const GNF = 'GNF';
+    const GTQ = 'GTQ';
+    const GYD = 'GYD';
+    const HNL = 'HNL';
+    const HRK = 'HRK';
+    const HTG = 'HTG';
+    const HUF = 'HUF';
+    const IDR = 'IDR';
+    const ILS = 'ILS';
+    const IMP = 'IMP';
+    const IQD = 'IQD';
+    const IRR = 'IRR';
+    const ISK = 'ISK';
+    const JEP = 'JEP';
+    const JMD = 'JMD';
+    const JOD = 'JOD';
+    const KGS = 'KGS';
+    const KHR = 'KHR';
+    const KID = 'KID';
+    const KMF = 'KMF';
+    const KWD = 'KWD';
+    const KYD = 'KYD';
+    const KZT = 'KZT';
+    const LAK = 'LAK';
+    const LBP = 'LBP';
+    const LKR = 'LKR';
+    const LRD = 'LRD';
+    const LSL = 'LSL';
+    const LYD = 'LYD';
+    const MAD = 'MAD';
+    const MDL = 'MDL';
+    const MGA = 'MGA';
+    const MKD = 'MKD';
+    const MMK = 'MMK';
+    const MNT = 'MNT';
+    const MOP = 'MOP';
+    const MRU = 'MRU';
+    const MUR = 'MUR';
+    const MVR = 'MVR';
+    const MWK = 'MWK';
+    const MYR = 'MYR';
+    const MZN = 'MZN';
+    const NAD = 'NAD';
+    const NGN = 'NGN';
+    const NIO = 'NIO';
+    const NPR = 'NPR';
+    const OMR = 'OMR';
+    const PAB = 'PAB';
+    const PEN = 'PEN';
+    const PGK = 'PGK';
+    const PHP = 'PHP';
+    const PKR = 'PKR';
+    const PLN = 'PLN';
+    const PYG = 'PYG';
+    const QAR = 'QAR';
+    const RON = 'RON';
+    const RSD = 'RSD';
+    const RWF = 'RWF';
+    const SAR = 'SAR';
+    const SBD = 'SBD';
+    const SCR = 'SCR';
+    const SDG = 'SDG';
+    const SHP = 'SHP';
+    const SLE = 'SLE';
+    const SLL = 'SLL';
+    const SOS = 'SOS';
+    const SRD = 'SRD';
+    const SSP = 'SSP';
+    const STN = 'STN';
+    const SVC = 'SVC';
+    const SYP = 'SYP';
+    const SZL = 'SZL';
+    const THB = 'THB';
+    const TJS = 'TJS';
+    const TMT = 'TMT';
+    const TND = 'TND';
+    const TOP = 'TOP';
+    const TTD = 'TTD';
+    const TVD = 'TVD';
+    const TWD = 'TWD';
+    const TZS = 'TZS';
+    const UAH = 'UAH';
+    const UGX = 'UGX';
+    const UYU = 'UYU';
+    const UYW = 'UYW';
+    const UZS = 'UZS';
+    const VES = 'VES';
+    const VND = 'VND';
+    const VUV = 'VUV';
+    const WST = 'WST';
+    const XAF = 'XAF';
+    const XCD = 'XCD';
+    const XDR = 'XDR';
+    const XOF = 'XOF';
+    const XPF = 'XPF';
+    const YER = 'YER';
+    const ZMW = 'ZMW';
+    const ZWL = 'ZWL';
+
+    public static function values(): array
+    {
+        return [
+            self::USD,
+            self::KES,
+            self::GBP,
+            self::CAD,
+            self::EUR,
+            self::JPY,
+            self::AUD,
+            self::CHF,
+            self::CNY,
+            self::SEK,
+            self::NZD,
+            self::MXN,
+            self::SGD,
+            self::HKD,
+            self::NOK,
+            self::KRW,
+            self::TRY,
+            self::RUB,
+            self::INR,
+            self::BRL,
+            self::ZAR,
+            self::AED,
+            self::AFN,
+            self::ALL,
+            self::AMD,
+            self::ANG,
+            self::AOA,
+            self::ARS,
+            self::AWG,
+            self::AZN,
+            self::BAM,
+            self::BBD,
+            self::BDT,
+            self::BGN,
+            self::BHD,
+            self::BIF,
+            self::BMD,
+            self::BND,
+            self::BOB,
+            self::BOV,
+            self::BSD,
+            self::BTN,
+            self::BWP,
+            self::BYN,
+            self::BZD,
+            self::CDF,
+            self::CHE,
+            self::CHW,
+            self::CLF,
+            self::CLP,
+            self::COP,
+            self::COU,
+            self::CRC,
+            self::CUC,
+            self::CUP,
+            self::CVE,
+            self::CZK,
+            self::DJF,
+            self::DKK,
+            self::DOP,
+            self::DZD,
+            self::EGP,
+            self::ERN,
+            self::ETB,
+            self::FJD,
+            self::FKP,
+            self::FOK,
+            self::GEL,
+            self::GGP,
+            self::GHS,
+            self::GIP,
+            self::GMD,
+            self::GNF,
+            self::GTQ,
+            self::GYD,
+            self::HNL,
+            self::HRK,
+            self::HTG,
+            self::HUF,
+            self::IDR,
+            self::ILS,
+            self::IMP,
+            self::IQD,
+            self::IRR,
+            self::ISK,
+            self::JEP,
+            self::JMD,
+            self::JOD,
+            self::KGS,
+            self::KHR,
+            self::KID,
+            self::KMF,
+            self::KWD,
+            self::KYD,
+            self::KZT,
+            self::LAK,
+            self::LBP,
+            self::LKR,
+            self::LRD,
+            self::LSL,
+            self::LYD,
+            self::MAD,
+            self::MDL,
+            self::MGA,
+            self::MKD,
+            self::MMK,
+            self::MNT,
+            self::MOP,
+            self::MRU,
+            self::MUR,
+            self::MVR,
+            self::MWK,
+            self::MYR,
+            self::MZN,
+            self::NAD,
+            self::NGN,
+            self::NIO,
+            self::NPR,
+            self::OMR,
+            self::PAB,
+            self::PEN,
+            self::PGK,
+            self::PHP,
+            self::PKR,
+            self::PLN,
+            self::PYG,
+            self::QAR,
+            self::RON,
+            self::RSD,
+            self::RWF,
+            self::SAR,
+            self::SBD,
+            self::SCR,
+            self::SDG,
+            self::SHP,
+            self::SLE,
+            self::SLL,
+            self::SOS,
+            self::SRD,
+            self::SSP,
+            self::STN,
+            self::SVC,
+            self::SYP,
+            self::SZL,
+            self::THB,
+            self::TJS,
+            self::TMT,
+            self::TND,
+            self::TOP,
+            self::TTD,
+            self::TVD,
+            self::TWD,
+            self::TZS,
+            self::UAH,
+            self::UGX,
+            self::UYU,
+            self::UYW,
+            self::UZS,
+            self::VES,
+            self::VND,
+            self::VUV,
+            self::WST,
+            self::XAF,
+            self::XCD,
+            self::XDR,
+            self::XOF,
+            self::XPF,
+            self::YER,
+            self::ZMW,
+            self::ZWL,
+        ];
+    }
+}
