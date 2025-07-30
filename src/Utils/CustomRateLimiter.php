92f667a646ab8fb478b81bec1710ca498fc2e4c4
Muheez Jimoh <jamuheez2009@gmail.com>
2025-07-30 14:26:25 +0100

    Added A customratelimiter

---
+++ src/Utils/CustomRateLimiter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shekel\ShekelLib\Utils;
+
+use Illuminate\Http\Request;
+
+class CustomRateLimiter
+{
+    public static function getRateLimitKey(Request $request)
+    {
+        $by = $request->user()?->id ?: $request->ip();
+        if (!empty($request->bearerToken())) {
+            $user = PassportToken::getUserFromToken($request->bearerToken());
+            $by = sha1($user['user_id'] . $request->ip());
+        }
+        return $by;
+    }
+}
