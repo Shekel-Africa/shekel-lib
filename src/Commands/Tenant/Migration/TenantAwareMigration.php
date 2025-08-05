2d0af06b788817656074133c8be8ff229bcd9ac1
Muheez Jimoh <jamuheez2009@gmail.com>
2025-08-05 11:55:38 +0100

    Added tenantawaremigration to migrate all the tenant connections

---
+++ src/Commands/Tenant/Migration/TenantAwareMigration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Shekel\ShekelLib\Commands\Tenant\Migration;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Shekel\ShekelLib\Commands\Tenant\TenantAwareCommands;
+
+class TenantAwareMigration extends TenantAwareCommands
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:tenant:all
+                {--database= : The database connection to use}
+                {--force : Force the operation to run when in production}
+                {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+                {--schema-path= : The path to a schema dump file}
+                {--pretend : Dump the SQL queries that would be run}
+                {--seed : Indicates if the seed task should be re-run}
+                {--seeder= : The class name of the root seeder}
+                {--step : Force the migrations to be run so they can be rolled back individually}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run Migration for all tenants';
+
+
+    public function handleCommand(): mixed
+    {
+        $this->call('migrate', Arr::prependKeysWith($this->options(), '--'));
+        return Command::SUCCESS;
+    }
+}
